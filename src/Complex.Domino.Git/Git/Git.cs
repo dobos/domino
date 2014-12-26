b85cbaa8078b9279dc4e6ab885a0da9a926b4cdd
Laszlo Dobos <dobos@complex.elte.hu>
2014-12-26 01:22:38 +0100

    git init function implemented.

--- src/Complex.Domino.Git/Git/Git.cs
+++ src/Complex.Domino.Git/Git/Git.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using System.Configuration;
+using System.Diagnostics;
 
 namespace Complex.Domino.Git
 {
     public class Git
     {
+        #region Static members
+
         public static GitConfiguration Configuration
         {
             get
@@ -17,5 +21,85 @@ namespace Complex.Domino.Git
             }
         }
 
+        #endregion
+        #region Private member variables
+
+        private string binPath;
+        private string repoPath;
+
+        #endregion
+        #region Properties
+
+        public string BinPath
+        {
+            get { return binPath; }
+            set { binPath = value; }
+        }
+
+        public string RepoPath
+        {
+            get { return repoPath; }
+            set { repoPath = value; }
+        }
+
+        #endregion
+        #region Constructors and initializers
+
+        public Git()
+        {
+            InitializeMembers();
+        }
+
+        public Git(string repoPath)
+            : this()
+        {
+            this.repoPath = repoPath;
+        }
+
+        private void InitializeMembers()
+        {
+            this.binPath = Configuration.BinPath;
+            this.repoPath = ".";
+        }
+
+        #endregion
+        #region Git wrapper functions
+
+        public void Init()
+        {
+            CallGit("init");
+        }
+
+        #endregion
+        #region Process activation functions
+
+        private void CallGit(string arguments)
+        {
+            var pinfo = new ProcessStartInfo()
+            {
+                FileName = Path.Combine(binPath, "git.exe"),
+                WorkingDirectory = repoPath,
+                Arguments = arguments,
+
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                LoadUserProfile = false,
+            };
+
+            var proc = new Process()
+            {
+                StartInfo = pinfo,
+            };
+
+            proc.Start();
+
+            proc.WaitForExit();
+        }
+
+        #endregion
     }
 }
---
+++ test/Complex.Domino.Git.Test/Git/GitTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Complex.Domino.Git
+{
+    [TestClass]
+    public class GitTest
+    {
+        protected string TestRepoPath
+        {
+            get { return Path.GetFullPath("testrepo"); }
+        }
+
+        [TestInitialize()]
+        public void Initialize() 
+        {
+            // Create directory for empty repo
+            var repopath = TestRepoPath;
+
+            if (Directory.Exists(repopath))
+            {
+                Directory.Delete(repopath, true);
+            }
+
+            Directory.CreateDirectory(repopath);
+        }
+
+        [TestCleanup()]
+        public void Cleanup() 
+        {
+            var repopath = TestRepoPath;
+
+            if (Directory.Exists(repopath))
+            {
+                Directory.Delete(repopath, true);
+            }
+        }
+
+        [TestMethod]
+        public void InitTest()
+        {
+            var git = new Git(TestRepoPath);
+
+            git.Init();
+
+            Assert.IsTrue(Directory.Exists(Path.Combine(TestRepoPath, ".git")));
+        }
+    }
+}
