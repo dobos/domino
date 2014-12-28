47501660ddf8dc05a4fecdcfb2696567d780e057
Laszlo Dobos <dobos@complex.elte.hu>
2014-12-28 13:16:19 +0100

    Lib test addded.

---
+++ test/Complex.Domino.Lib.Test/Lib/UserTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Complex.Domino.Lib
+{
+    [TestClass]
+    public class UserTest
+    {
+        [TestMethod]
+        public void HashPasswordTest()
+        {
+            using (var context = Context.Create())
+            {
+                var u = new User(context);
+
+                u.SetPassword("alma");
+            }
+        }
+    }
+}
