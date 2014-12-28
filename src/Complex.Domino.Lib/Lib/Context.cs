68a9c40308e7cd4ac8011a84f3527d5c1e43c2a5
Laszlo Dobos <dobos@complex.elte.hu>
2014-12-28 13:14:58 +0100

    Context implemented.

--- src/Complex.Domino.Lib/Lib/Context.cs
+++ src/Complex.Domino.Lib/Lib/Context.cs
@@ -9,28 +9,182 @@ using System.Data.SqlClient;
 
 namespace Complex.Domino.Lib
 {
-    public class Context
+    public class Context : IDisposable
     {
+        #region Private member variables
+
+        private bool isValid;
+
+        private int userid;
+        private string username;
+
+        private SqlConnection databaseConnection;
+        private SqlTransaction databaseTransaction;
+
+        #endregion
+
         private string ConnectionString
         {
             get { return ConfigurationManager.ConnectionStrings["Domino"].ConnectionString; }
         }
 
+        public int UserID
+        {
+            get { return userid; }
+            set { userid = value; }
+        }
+
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        #region Contructors and initializers
+
+        private Context()
+        {
+        }
+
+        public static Context Create()
+        {
+            return new Context();
+        }
+
+        private void InitializeMembers()
+        {
+            this.userid = -1;
+            this.username = null;
+            this.databaseConnection = null;
+            this.databaseTransaction = null;
+        }
+
+        public void Dispose()
+        {
+
+            if (databaseTransaction != null)
+            {
+                CommitTransaction();
+            }
+
+            if (databaseConnection != null)
+            {
+                CloseConnection();
+            }
+
+            isValid = false;
+        }
+
+        #endregion
+        #region Connection and transaction functions
+
+        private void EnsureOpenConnection()
+        {
+            if (!isValid)
+            {
+                throw new InvalidOperationException();
+            }
+
+            if (databaseConnection == null)
+            {
+                OpenConnection();
+            }
+        }
+
         public SqlConnection OpenConnection()
         {
-            var cn = new SqlConnection(ConnectionString);
-            cn.Open();
+            databaseConnection = new SqlConnection(ConnectionString);
+            databaseConnection.Open();
 
-            return cn;
+            return databaseConnection;
         }
+
+        private void CloseConnection()
+        {
+            if (databaseConnection != null)
+            {
+                if (databaseConnection.State != ConnectionState.Closed)
+                {
+                    databaseConnection.Close();
+                }
+                databaseConnection.Dispose();
+                databaseConnection = null;
+            }
+        }
+
+        private void EnsureOpenTransaction()
+        {
+            if (!isValid)
+            {
+                throw new InvalidOperationException();
+            }
+
+            if (databaseTransaction == null)
+            {
+                BeginTransaction();
+            }
+        }
+
+        private SqlTransaction BeginTransaction()
+        {
+            EnsureOpenConnection();
+
+            if (databaseTransaction == null)
+            {
+                databaseTransaction = databaseConnection.BeginTransaction();
+            }
+
+            return databaseTransaction;
+        }
+
+        /// <summary>
+        /// Commits the current SQL Server transaction.
+        /// </summary>
+        public void CommitTransaction()
+        {
+            if (!isValid)
+            {
+                throw new InvalidOperationException();
+            }
+
+            if (databaseTransaction != null)
+            {
+                databaseTransaction.Commit();
+                databaseTransaction.Dispose();
+                databaseTransaction = null;
+            }
+        }
+
+        /// <summary>
+        /// Rolls back the current SQL Server transaction.
+        /// </summary>
+        public void RollbackTransaction()
+        {
+            if (!isValid)
+            {
+                throw new InvalidOperationException();
+            }
+
+            if (databaseTransaction != null)
+            {
+                databaseTransaction.Rollback();
+                databaseTransaction.Dispose();
+                databaseTransaction = null;
+            }
+        }
+
+        #endregion
+        #region Query execution helpers
 
         public IEnumerable<T> ExecuteCommandReader<T>(SqlCommand cmd)
             where T : IDatabaseTableObject, new()
         {
-            var cn = OpenConnection();
+            EnsureOpenConnection();
+            EnsureOpenTransaction();
 
-            cmd.Connection = cn;
-            cmd.CommandTimeout = 30;
+            cmd.Connection = databaseConnection;
+            cmd.Transaction = databaseTransaction;
+            cmd.CommandTimeout = 30;        // *** TODO
 
             var dr = cmd.ExecuteReader(CommandBehavior.SequentialAccess | CommandBehavior.CloseConnection);
 
@@ -39,11 +193,12 @@ namespace Complex.Domino.Lib
 
         public void ExecuteCommandNonQuery(SqlCommand cmd)
         {
-            using (var cn = OpenConnection())
-            {
-                cmd.Connection = cn;
-                cmd.ExecuteNonQuery();
-            }
+            EnsureOpenConnection();
+            EnsureOpenTransaction();
+
+            cmd.Connection = databaseConnection;
+            cmd.Transaction = databaseTransaction;
+            cmd.ExecuteNonQuery();
         }
 
         public string[] SplitQuery(string sql)
@@ -80,5 +235,6 @@ namespace Complex.Domino.Lib
             }
         }
 
+        #endregion
     }
 }
