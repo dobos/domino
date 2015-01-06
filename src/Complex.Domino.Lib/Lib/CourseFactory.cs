8f9f0c9b282e8af42e849897a0b49b5cabaf60ee
Laszlo Dobos <dobos@complex.elte.hu>
2015-01-06 08:14:34 -0500

    Course list added.

---
+++ src/Complex.Domino.Lib/Lib/CourseFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complex.Domino.Lib
+{
+    public class CourseFactory : EntityFactory
+    {
+        protected override string TableName
+        {
+            get { return "Course"; }
+        }
+
+        public CourseFactory(Context context)
+            :base(context)
+        {
+            InitializeMembers();
+        }
+
+        private void InitializeMembers()
+        {
+
+        }
+
+        public IEnumerable<Course> Find()
+        {
+            return Find(-1, -1);
+        }
+
+        public IEnumerable<Course> Find(int max, int from)
+        {
+            return base.Find<Course>(max, from);
+        }
+    }
+}
---
+++ src/Complex.Domino.Web/Admin/CourseList.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Complex.Domino.Web.Admin
+{
+    public partial class CourseList : PageBase
+    {
+        private Lib.CourseFactory searchObject;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ToolbarCreate.NavigateUrl = Semester.GetUrl();
+        }
+
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            if (courseList.Visible)
+            {
+                courseList.DataBind();
+            }
+        }
+
+        protected void courseDataSource_ObjectCreating(object sender, ObjectDataSourceEventArgs e)
+        {
+            searchObject = new Lib.CourseFactory(DatabaseContext);
+
+            // TODO: set search criteria here
+
+            e.ObjectInstance = searchObject;
+        }
+    }
+}
