d0bf77b392da87cd45c215f198b1e93e5c4ca0d7
Laszlo Dobos <dobos@complex.elte.hu>
2015-01-09 17:48:57 -0500

    Top level teacher pages added.

---
+++ src/Complex.Domino.Web/Teacher/Course.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Complex.Domino.Web.Teacher
+{
+    public partial class Course : EntityForm<Lib.Course>
+    {
+        public static string GetUrl()
+        {
+            return "~/Teacher/Course.aspx";
+        }
+
+        protected override void UpdateForm()
+        {
+            base.UpdateForm();
+
+            Semester.Text = Item.SemesterID.ToString();
+            StartDate.Text = Item.StartDate.ToString();
+            EndDate.Text = Item.EndDate.ToString();
+            Url.Text = Item.Url;
+            // TODO = Item.HtmlPage;
+            GradeType.SelectedValue = Item.GradeType.ToString();
+        }
+
+        protected override void SaveForm()
+        {
+            base.SaveForm();
+
+            Item.Url = Url.Text;
+            // Item.HtmlPage = // TODO
+            Item.GradeType = (Lib.GradeType)Enum.Parse(typeof(Lib.GradeType), GradeType.SelectedValue);
+        }
+    }
+}
---
+++ src/Complex.Domino.Web/Teacher/CourseList.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Complex.Domino.Web.Teacher
+{
+    public partial class CourseList : PageBase
+    {
+        public static string GetUrl()
+        {
+            return "~/Teacher/CourseList.aspx";
+        }
+
+        private Lib.CourseFactory searchObject;
+
+
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            if (courseList.Visible)
+            {
+                courseList.DataBind();
+            }
+        }
+
+        protected void courseDataSource_ObjectCreating(object sender, ObjectDataSourceEventArgs e)
+        {
+            searchObject = new Lib.CourseFactory(DatabaseContext);
+
+            // Set search criteria
+            if (Request.QueryString[Constants.RequestSemesterID] != null)
+            {
+                searchObject.SemesterID = int.Parse(Request.QueryString[Constants.RequestSemesterID]);
+            }
+
+            e.ObjectInstance = searchObject;
+        }
+    }
+}
