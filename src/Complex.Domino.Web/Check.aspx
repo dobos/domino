d0e26f153fa0ea8f6d2a71161db5fa41a93dd2d3
Laszlo Dobos <dobos@complex.elte.hu>
2018-12-09 13:02:56 +0100

    Added email server check routine.

---
+++ src/Complex.Domino.Web/Check.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Check.aspx.cs" Inherits="Complex.Domino.Web.Check" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <pre><% RunChecks(); %></pre>
+        </div>
+    </form>
+</body>
+</html>
---
+++ src/Complex.Domino.Web/Check.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Complex.Domino;
+
+namespace Complex.Domino.Web
+{
+    public partial class Check : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void RunChecks()
+        {
+            try
+            {
+                Page.Response.Output.WriteLine("Sending e-mail to {0}...", Lib.DominoConfiguration.Instance.EmailFromAddress);
+
+                Util.Email.SendFromDomino(
+                    Lib.DominoConfiguration.Instance.EmailFromName,
+                    Lib.DominoConfiguration.Instance.EmailFromAddress,
+                    "Domino Test",
+                    "Test");
+
+                Page.Response.Output.WriteLine("OK");
+            }
+            catch (Exception ex)
+            {
+                Page.Response.Output.WriteLine("Error: {0}", ex.Message);
+                
+            }
+        }
+    }
+}
